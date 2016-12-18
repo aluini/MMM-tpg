77fe8ab53b7784c984d552764118b46211643425
Alexandre Luini <alexandre.luini@b-i.com>
2016-12-18 17:15:58 +0100

    Initial Release

--- README.md
+++ README.md
@@ -1,2 +1,76 @@
 # MMM-tpg
+
 MagicMirror² module - TPG (Transport publics Genevois) station monitor
+
+This module is an extension of [MagicMirror
+
+<sup>2</sup>](https://github.com/MichMich/MagicMirror)
+
+ project by [MichMich](https://github.com/MichMich/)
+
+## Screenshots
+
+![Preview version](screenshots/screenshot.png)
+
+## Current version
+
+v1.0.0
+
+## Prerequisite
+
+A working installation of [MagicMirror
+
+<sup>2</sup>](https://github.com/MichMich/MagicMirror)
+
+## Installation
+
+Navigate into your MagicMirror's modules folder and execute <br>
+`git clone https://github.com/aluini/MMM-tpg.git`
+
+## Configuration
+
+Sample minimum configuration entry for your `~/MagicMirror/config/config.js`:
+
+```
+...
+
+{
+    module: 'tpg',
+    position: 'top_left',
+    config: {
+      params: {
+        stopCode: '',            // Which stop would you like to have displayed?            
+        key: ''  // Add your tpg opendata key
+      }
+    }
+} // If this isn't your last module, add a comma after the bracket
+
+...
+```
+
+Sample configuration entry for your `~/MagicMirror/config/config.js` with optional parameters:
+
+```
+...
+
+{
+    module: 'tpg',
+    position: 'top_left',
+    config: {
+      params: {
+        stopCode: '',            // Which stop would you like to have displayed?            
+        key: ''  // Add your tpg opendata key
+      }
+      updateInterval: 300000, // How often should the information be updated? (In milliseconds)
+      maxDepartures: 10, // How many connections would you like to see?
+    }
+}
+
+...
+```
+
+## Figuring out the correct stopCode
+
+1. Open your web browser and navigate to the [<http://prod.ivtr-od.tpg.ch/v1/GetStops?key=xxxx]http://prod.ivtr-od.tpg.ch/v1/GetStops?key=xxxx>.
+2. replace xxxx by your tpg opendata key
+3. Copy the stopCode.
---
+++ tpg.js
@@ -0,0 +1,87 @@
+/* Magic Mirror
+ * Module: tpg
+ *
+ * By Alexandre LUINI http://github.com/aluini
+ * MIT Licensed.
+ */
+Module.register('tpg', {
+  // Module config defaults.
+  defaults: {
+    updateInterval: 300000, // 5min
+    maxDepartures: 10, // max 10 entries
+    api: {
+      base: 'http://prod.ivtr-od.tpg.ch/v1/',
+      method: 'GetNextDepartures'
+    },
+    params: {
+      stopCode: '',
+      key: ''
+    }
+  },
+  // get jQuery
+  getScripts: function () {
+    return [this.file('js/jquery.js')]
+  },
+  // get styles
+  getStyles: function () {
+    return ['font-awesome.css', this.file('css/tpg.css')]
+  },
+  // init method
+  start: function () {
+    Log.info('Starting module: ' + this.name)
+    this.result = false
+    this.url = this.config.api.base + this.config.api.method + '?' + $.param(this.config.params)
+    this.getData()
+    console.log(this)
+/*
+    setInterval(() => {
+        this.getData();
+    }, this.config.api.updateInterval)*/
+  },
+  // Override dom generator.
+  getDom: function () {
+    var wrapper = document.createElement('div')
+    if (this.result) {
+      var table = document.createElement('table')
+      table.classList.add('small', 'table', 'align-left')
+
+      var departures = this.result.departures
+      var maxDeparture = this.config.maxDepartures
+      departures.forEach(function (departure, i) {
+        if (i < maxDeparture) {
+          var departure_row = document.createElement('tr')
+
+          var departure_vehiculType = document.createElement('td')
+          var vehiculType = document.createElement('i')
+          vehiculType.classList.add('fa', departure.vehiculeType === 'AB' || departure.vehiculeType === 'ABA' ? 'fa-bus' : 'fa-subway')
+          departure_vehiculType.appendChild(vehiculType)
+          departure_row.appendChild(departure_vehiculType)
+
+          var departure_name = document.createElement('td')
+          departure_name.innerHTML = departure.line.lineCode + ' ' + departure.line.destinationName
+          departure_row.appendChild(departure_name)
+
+          var departure_time = document.createElement('td')
+          departure_time.classList.add('centered')
+          departure_time.innerHTML = departure.waitingTime
+          departure_row.appendChild(departure_time)
+
+          if (departure.waitingTime !== 'no more') {
+            table.appendChild(departure_row)
+          }
+        }
+      })
+      wrapper.appendChild(table)
+    } else {
+      wrapper.innerHTML = 'Loading...'
+    }
+    return wrapper
+  },
+
+  getData: function () {
+    $.getJSON(this.url, (data) => {
+      this.result = data
+      this.updateDom()
+    })
+  }
+})
